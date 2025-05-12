c5c850d223844f006fdb061f6a766af00ffacfa0
WalterLandaburo <walter.libreria@gmail.com>
2025-05-12 19:07:16 -0300

    Add files via upload
    Bloque de código para usar for.

---
+++ ForBloqueDeCodigo.py
@@ -0,0 +1,11 @@
+#Muestra del bloque de código for ---
+print("Bloque de código")
+print("for variable in secuencia")
+
+
+
+#Ejemplo del uso de for -------------
+productos = ["manzanas", "naranjas", "bananas", "peras"]
+
+for producto in productos:
+    print("Producto disponible:", producto)
